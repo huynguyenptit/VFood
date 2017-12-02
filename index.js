22e270622a16f8e0ace6a4e2cf374eaec3b55d93
huynguyenptit <huynguyenptit97@gmail.com>
2017-12-02 17:25:17 +0700

    theem trang chi tiet

--- index.js
+++ index.js
@@ -12,18 +12,18 @@ async function launchBrowser(){
 async function run() {
     const browser = await launchBrowser();
     const page = await browser.newPage();
-    await page.goto('https://www.foody.vn');
+    await page.goto('https://www.foody.vn/ho-chi-minh/hanuri-quan-an-han-quoc-dien-bien-phu/goi-mon');
     await sleep(10000);
     const html = await page.evaluate(() => {
         const html = document.querySelector('html').outerHTML;
         return html;
     });
-    fs.writeFile('trangchu.html',html, function (err) {
+    fs.writeFile('goi-mon-hanuri-quan-an-han-quoc-dien-bien-phu.html',html, function (err) {
         if (err) throw err;
         console.log('Saved!');
     });
 }
-// run();
+run();
 function sleep(ms) {
     return new Promise(
         function(resolve, reject) {
