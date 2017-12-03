0f3eea7f886f4c30de617ffa2fa5d4b277b1cd7c
huynguyenptit <huynguyenptit97@gmail.com>
2017-12-03 20:15:45 +0700

    Hoàn thiện trang chi tiết, đăng nhập

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ async function launchBrowser(){
 async function run() {
     const browser = await launchBrowser();
     const page = await browser.newPage();
-    await page.goto('https://www.foody.vn/ho-chi-minh/hanuri-quan-an-han-quoc-dien-bien-phu/goi-mon');
+    await page.goto('https://www.foody.vn/ho-chi-minh/hanuri-quan-an-han-quoc-dien-bien-phu/binh-luan');
     await sleep(10000);
     const html = await page.evaluate(() => {
         const html = document.querySelector('html').outerHTML;
         return html;
     });
-    fs.writeFile('goi-mon-hanuri-quan-an-han-quoc-dien-bien-phu.html',html, function (err) {
+    fs.writeFile('comment-hanuri-quan-an-han-quoc-dien-bien-phu.html',html, function (err) {
         if (err) throw err;
         console.log('Saved!');
     });
